18d21e646fb2511da988437d831d07f9dabd15c5
suzuki-kaito <uminto532@gmail.com>
2019-09-23 20:46:49 +0900

    Asynchronous
    非同期通信を実装しました

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,49 @@
+document.addEventListener('turbolinks:load', function() {
+
+  $('#new_message').on('submit', function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    href = window.location.href
+
+    $.ajax({
+      type: 'POST',
+      url: href,
+      data: formData,
+      processData: false,
+      contentType: false,
+      dataType: 'json'
+    })
+    .done(function(data) {
+      // ScrollDown()
+      var html = messageBuildHTML(data);
+      $('.messages').append(html);
+      $('#new_message')[0].reset();
+    })
+    .fail(function(data) {
+      alert('error');
+    });
+    return false;
+  });
+  function messageBuildHTML(message) {
+    var html = `
+    <div class='message'>
+<div class='upper-message'>
+<div class='upper-message__user-name'>
+${message.name}
+</div>
+<div class='upper-message__date'>
+${message.time}
+</div>
+</div>
+<div class='lower-message'>
+<p class='lower-message__content'>
+${message.content}
+</p>
+
+</div>
+</div>
+    `
+    return html
+  }
+});
+
